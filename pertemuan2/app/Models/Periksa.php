9f1239d3bff70e34a76cecb449c25776ef913cd6
AenurHakimdesu <hakimhaya29@gmail.com>
2025-03-24 07:35:06 +0700

    upload tugas2 laravel

---
+++ pertemuan2/app/Models/Periksa.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Periksa extends Model
+{
+    protected $fillable = [
+        'id_pasien',
+        'id_dokter',
+        'tgl_periksa',
+        'catatan',
+        'biaya_periksa',
+    ];
+
+    public function pasien(){
+        return $this->belongsTo(user::class, 'id_pasien');
+    }
+    public function dokter(){
+        return $this->belongsTo(user::class, 'id_dokter');
+    }
+    public function detail_periksa(){
+        return $this->hasMany(DetailPeriksa::class, 'id_periksa', 'id');
+    }
+}
